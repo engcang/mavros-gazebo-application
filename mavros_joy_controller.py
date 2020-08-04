40e5e79e7476b3bb753a84ff099e19be1ef0fa48
EUNGCHANG-MASON <eungchang_mason@kaist.ac.kr>
2020-08-04 22:02:20 +0900

    Update mavros_joy_controller.py

--- mavros_joy_controller.py
+++ mavros_joy_controller.py
@@ -7,7 +7,7 @@ Created on Tue Feb 26 02:02:03 2019
 
 ''' import libraries '''
 import time
-from tf.transformations import euler_from_quaternion
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from math import cos, sin
 import numpy as np
 
@@ -16,6 +16,7 @@ from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import TwistStamped
 from sensor_msgs.msg import Joy
 from mavros_msgs.srv import SetMode, CommandBool
+from mavros_msgs.msg import AttitudeTarget
 
 import sys
 import signal
@@ -28,9 +29,13 @@ signal.signal(signal.SIGINT, signal_handler)
 
 global joy_check
 joy_check=0
+global mav_check
+mav_check=0
 
 global d2r
 global r2d
+global max_ang_x
+global max_ang_y
 global max_vel_x
 global max_vel_y
 global max_vel_z
@@ -39,26 +44,30 @@ yaw_rate = 2
 
 r2d = 180/np.pi
 d2r = np.pi/180
-max_ang_x = 5
-max_ang_y = 5
-max_ang_z = 5
+max_ang_x = 30 * d2r
+max_ang_y = 30 * d2r
+max_vel_x = 12
+max_vel_y = 12
+max_vel_z = 12
 
 ''' class '''
 class robot():
     def __init__(self):
         rospy.init_node('robot_controller', anonymous=True)
         self.local_vel_pub = rospy.Publisher('/mavros/setpoint_velocity/cmd_vel', TwistStamped, queue_size=100)
+        self.angle_pub = rospy.Publisher('/mavros/setpoint_raw/attitude', AttitudeTarget, queue_size=100)
         self.pos_sub = rospy.Subscriber('/mavros/local_position/pose', PoseStamped, self.pose_callback)
         self.joy_sub = rospy.Subscriber('/joy', Joy, self.joy_callback)
         self.arming = rospy.ServiceProxy('/mavros/cmd/arming', CommandBool)
         self.offboarding = rospy.ServiceProxy('/mavros/set_mode', SetMode)
 
         self.rate = rospy.Rate(30)
-
-        self.joy = Joy()
         self.mode = 2 #default is mode 2
+        self.mission = 0
 
     def pose_callback(self, msg):
+        global mav_check
+        mav_check=1
         self.truth=msg.pose.position
         orientation_list = [msg.pose.orientation.x, msg.pose.orientation.y, msg.pose.orientation.z, msg.pose.orientation.w]
         (self.roll, self.pitch, self.yaw) = euler_from_quaternion(orientation_list)
@@ -68,6 +77,9 @@ class robot():
         global joy_check
         if len(self.joy.axes)>0 or len(self.joy.buttons)>0 :
             joy_check=1
+            if self.joy.buttons[1]==1:
+                self.mission = self.mission+1
+                #rospy.set_param('/mavros/setpoint_attitude/reverse_thrust', True)
             if self.joy.buttons[4]==1:
                 self.mode=1
             if self.joy.buttons[5]==1:
@@ -82,46 +94,114 @@ def input(rbt):
     global r2d
     global max_ang_x
     global max_ang_y
+    global max_vel_x
+    global max_vel_y
+    global max_vel_z
+    global yaw_rate
 
-    vel_input=TwistStamped()
-##Mode 2, default
-#joy_axes: {pitch: 4, roll: 3, yaw: 0, vertical: 1}
-    if rbt.mode==2:
-        vel_input.twist.linear.x= ( rbt.joy.axes[4]*max_ang_x)*cos(rbt.yaw) - ( rbt.joy.axes[3]*max_ang_y)*sin(rbt.yaw)
-        vel_input.twist.linear.y= ( rbt.joy.axes[3]*max_ang_y)*cos(rbt.yaw) + ( rbt.joy.axes[4]*max_ang_x)*sin(rbt.yaw)
-        vel_input.twist.linear.z= ( rbt.joy.axes[1]*max_ang_z)
-        vel_input.twist.angular.z = yaw_rate*(rbt.joy.axes[0])
-##Mode 1
-#joy_axes: {pitch: 1, roll: 3, yaw: 0, vertical: 4}
-    elif rbt.mode==1:
-        vel_input.twist.linear.x= ( rbt.joy.axes[1]*max_ang_x)*cos(rbt.yaw) - ( rbt.joy.axes[3]*max_ang_y)*sin(rbt.yaw)
-        vel_input.twist.linear.y= ( rbt.joy.axes[3]*max_ang_y)*cos(rbt.yaw) + ( rbt.joy.axes[1]*max_ang_x)*sin(rbt.yaw)
-        vel_input.twist.linear.z= ( rbt.joy.axes[4]*max_ang_z)
-        vel_input.twist.angular.z = yaw_rate*(rbt.joy.axes[0])
+    if rbt.mission%3 == 0: #velocity mode
+        vel_input=TwistStamped()
 
-    print("Mode < %d > now, press L1 or R1 to change, Press Button[2] to arm/disarm"%rbt.mode)
-    print("Input : X: %.2f  Y: %.2f  Z: %.2f  Yaw: %.2f "%(vel_input.twist.linear.x, vel_input.twist.linear.y, vel_input.twist.linear.z, vel_input.twist.angular.z))
-    print("Position(Meter): X: %.2f Y: %.2f Z: %.2f "%(rbt.truth.x, rbt.truth.y, rbt.truth.z))
-    print("Angle(Degree): roll: %.2f pitch: %.2f yaw: %.2f \n"%(rbt.roll/np.pi*180, rbt.pitch/np.pi*180, rbt.yaw/np.pi*180)) #radian : +-pi
+        ##Mode 2, default
+        #joy_axes: {pitch: 4, roll: 3, yaw: 0, vertical: 1}
+        if rbt.mode==2:
+            vel_input.twist.linear.x= ( rbt.joy.axes[4]*max_vel_x)*cos(rbt.yaw) - ( rbt.joy.axes[3]*max_vel_y)*sin(rbt.yaw)
+            vel_input.twist.linear.y= ( rbt.joy.axes[3]*max_vel_y)*cos(rbt.yaw) + ( rbt.joy.axes[4]*max_vel_x)*sin(rbt.yaw)
+            vel_input.twist.linear.z= ( rbt.joy.axes[1]*max_vel_z)
+            vel_input.twist.angular.z = yaw_rate*(rbt.joy.axes[0])
+        ##Mode 1
+        #joy_axes: {pitch: 1, roll: 3, yaw: 0, vertical: 4}
+        elif rbt.mode==1:
+            vel_input.twist.linear.x= ( rbt.joy.axes[1]*max_vel_x)*cos(rbt.yaw) - ( rbt.joy.axes[3]*max_vel_y)*sin(rbt.yaw)
+            vel_input.twist.linear.y= ( rbt.joy.axes[3]*max_vel_y)*cos(rbt.yaw) + ( rbt.joy.axes[1]*max_vel_x)*sin(rbt.yaw)
+            vel_input.twist.linear.z= ( rbt.joy.axes[4]*max_vel_z)
+            vel_input.twist.angular.z = yaw_rate*(rbt.joy.axes[0])
 
-    vel_input.header.stamp = rospy.Time.now()
-    rbt.local_vel_pub.publish(vel_input)
+        print("Mode < %d > now, press L1 or R1 to change, Press Button[2],[3] to arm/disarm"%rbt.mode)
+        print("Mission:< %d >, Input : X: %.2f  Y: %.2f  Z: %.2f  Yaw: %.2f "%(rbt.mission%3, vel_input.twist.linear.x, vel_input.twist.linear.y, vel_input.twist.linear.z, vel_input.twist.angular.z))
+        print("Position(Meter): X: %.2f Y: %.2f Z: %.2f "%(rbt.truth.x, rbt.truth.y, rbt.truth.z))
+        print("Angle(Degree): roll: %.2f pitch: %.2f yaw: %.2f \n"%(rbt.roll/np.pi*180, rbt.pitch/np.pi*180, rbt.yaw/np.pi*180)) #radian : +-pi
+
+        vel_input.header.stamp = rospy.Time.now()
+        rbt.local_vel_pub.publish(vel_input)
+
+    elif rbt.mission%3 == 1: #angle mode
+        # rospy.set_param('/mavros/setpoint_attitude/reverse_thrust', True)
+        ang_input=AttitudeTarget()
+
+        ##Mode 2, default
+        #joy_axes: {pitch: 4, roll: 3, yaw: 0, vertical: 1}
+        if rbt.mode==2:
+            des_roll = -rbt.joy.axes[3]*max_ang_x
+            des_pitch = rbt.joy.axes[4]*max_ang_y
+            des_yaw = rbt.yaw + (rbt.joy.axes[0])
+            (x,y,z,w)=quaternion_from_euler(des_roll, des_pitch, des_yaw)
+            ang_input.orientation.x=x
+            ang_input.orientation.y=y
+            ang_input.orientation.z=z
+            ang_input.orientation.w=w
+            ang_input.thrust = 0.6 + (rbt.joy.axes[1])*0.4
+        ##Mode 1
+        #joy_axes: {pitch: 1, roll: 3, yaw: 0, vertical: 4}
+        elif rbt.mode==1:
+            des_roll = -rbt.joy.axes[3]*max_ang_x
+            des_pitch = rbt.joy.axes[1]*max_ang_y
+            des_yaw = rbt.yaw + (rbt.joy.axes[0])
+            (x,y,z,w)=quaternion_from_euler(des_roll, des_pitch, des_yaw)
+            ang_input.orientation.x=x
+            ang_input.orientation.y=y
+            ang_input.orientation.z=z
+            ang_input.orientation.w=w
+            ang_input.thrust = 0.6 + (rbt.joy.axes[4])*0.4
+
+        print("Mode < %d > now, press L1 or R1 to change, Press Button[2],[3] to arm/disarm"%rbt.mode)
+        print("Mission:< %d >, Input : R: %.2f  P: %.2f  Y: %.2f  thrust: %.2f "%(rbt.mission%3, des_roll*r2d, des_pitch*r2d, des_yaw*r2d, ang_input.thrust))
+        print("Position(Meter): X: %.2f Y: %.2f Z: %.2f "%(rbt.truth.x, rbt.truth.y, rbt.truth.z))
+        print("Angle(Degree): roll: %.2f pitch: %.2f yaw: %.2f \n"%(rbt.roll/np.pi*180, rbt.pitch/np.pi*180, rbt.yaw/np.pi*180)) #radian : +-pi
+
+        ang_input.header.stamp = rospy.Time.now()
+        rbt.angle_pub.publish(ang_input)
+
+    elif rbt.mission%3 == 2: #rate mode
+        ang_input=AttitudeTarget()
+
+        ##Mode 2, default
+        #joy_axes: {pitch: 4, roll: 3, yaw: 0, vertical: 1}
+        if rbt.mode==2:
+            ang_input.body_rate.x = -rbt.joy.axes[3]*max_ang_x
+            ang_input.body_rate.y = rbt.joy.axes[4]*max_ang_y
+            ang_input.body_rate.z = yaw_rate*(rbt.joy.axes[0])
+            ang_input.thrust = 0.6 + (rbt.joy.axes[1])*0.4
+        ##Mode 1
+        #joy_axes: {pitch: 1, roll: 3, yaw: 0, vertical: 4}
+        elif rbt.mode==1:
+            ang_input.body_rate.x = -rbt.joy.axes[3]*max_ang_x
+            ang_input.body_rate.y = rbt.joy.axes[1]*max_ang_y
+            ang_input.body_rate.z = yaw_rate*(rbt.joy.axes[0])
+            ang_input.thrust = 0.6 + (rbt.joy.axes[4])*0.4
+
+        print("Mode < %d > now, press L1 or R1 to change, Press Button[2],[3] to arm/disarm"%rbt.mode)
+        print("Mission:< %d >, Input : R: %.2f  P: %.2f  Y: %.2f  thrust: %.2f "%(rbt.mission%3, ang_input.body_rate.x*r2d, ang_input.body_rate.y*r2d, ang_input.body_rate.z*r2d, ang_input.thrust))
+        print("Position(Meter): X: %.2f Y: %.2f Z: %.2f "%(rbt.truth.x, rbt.truth.y, rbt.truth.z))
+        print("Angle(Degree): roll: %.2f pitch: %.2f yaw: %.2f \n"%(rbt.roll/np.pi*180, rbt.pitch/np.pi*180, rbt.yaw/np.pi*180)) #radian : +-pi
+
+        ang_input.header.stamp = rospy.Time.now()
+        rbt.angle_pub.publish(ang_input)
 
 ##############################################################################################
 
-alpha = robot()
-alpha.joy_callback(alpha.joy)
+mav_ctr = robot()
 time.sleep(1) #wait 1 second to assure that all data comes in
 
 ''' main '''
 if __name__ == '__main__':
     while 1:
         try:
-            if joy_check==1:
-                input(alpha)
-                alpha.rate.sleep()
+            if joy_check==1 and mav_check==1:
+                input(mav_ctr)
+                mav_ctr.rate.sleep()
             else: 
-                alpha.rate.sleep()
+                mav_ctr.rate.sleep()
                 pass
         except (rospy.ROSInterruptException, SystemExit, KeyboardInterrupt) :
             sys.exit(0)
